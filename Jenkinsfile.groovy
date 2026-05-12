2a6bae7f163c91cbc4e35c1c30034d69ff81a9dd
Developer Second <dev2@1.com>
2026-05-12 20:03:52 +0300

    123

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,34 @@
+/* groovylint-disable LineLength */
+pipeline {
+    agent {
+        label '1c-node'
+    }
+    environment {
+        envString = 'world'
+    }
+
+    post {
+        always {
+            allure commandline:'allure', includeProperties: false, jdk:'', resultPolicy: 'LEAVE_AS_IS', results: [[path:'out/syntax-check/allure']]
+        }
+        failure {
+            bat 'echo failure'
+        }
+        success {
+            bat 'echo succes'
+        }
+    }
+    stages {
+        stage('Build test base') {
+            steps {
+                bat 'chcp 65001\n vrunner init-dev --v8version 8.3.23.1912 --ibconnection /FC:\\repo\\jenkins-2025-12\\build\\ib --dt C:\\jenkins\\template\\1Cv8.dt --src C:\\repo\\jenkins-2025-12\\src\\cf'
+            }
+        }
+        stage('Syntax check') {
+            steps {
+                bat 'chcp 65001\n vrunner syntax-check'
+            }
+        }
+    }
+}
+
